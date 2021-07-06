1c4f4c447e53e2a07faa429377d947bac2c5490d
Amarpreet <amarpreetbhatia@gmail.com>
2021-07-06 23:58:11 +1000

    added exceptionally method on CompletableFuture example. Exceptionally function only calls when exception occured but handle call always in pipeline.

--- src/main/java/com/amarpreet/completableFuture/CompletableFutureHelloWorldException.java
+++ src/main/java/com/amarpreet/completableFuture/CompletableFutureHelloWorldException.java
@@ -12,7 +12,7 @@ public class CompletableFutureHelloWorldException {
         this.helloWorldService = helloWorldService;
     }
 
-    public String helloWorld_with_3_async(){
+    public String helloWorld_with_3_async_with_handle_func(){
         long startTime = System.currentTimeMillis();
         // Traditionally we call hello() and world() methods and Concatenate there results h + w
         CompletableFuture<String> hello = CompletableFuture
@@ -25,14 +25,54 @@ public class CompletableFutureHelloWorldException {
 
         String result = hello
                 .handle((res,e) -> {
-                    System.out.println("Exception occur::" + e.getMessage());
-                    return ""; // recover value, as handle() is recover
+                    if(e !=null){
+                        System.out.println("Exception occur::" + e.getMessage());
+                        return ""; // recover value, as handle() is recover
+                    }else{
+                        return res;
+                    }
+
                 })
                 .thenCombine(world, (h,w) -> h+w) // where h is the return of hello and w is the return of world
                 .handle((res,e) -> {
-                    System.out.println("Exception on world method failed occur::" + e.getMessage());
-                    return ""; // recover value, as handle() is recover
+                    if(e !=null){
+                        System.out.println("Exception on world method failed occur::" + e.getMessage());
+                        return ""; // recover value, as handle() is recover
+                    }else{
+                        return res; //handle is called, even for no exception occured
+                    }
+
                 })
+                .thenCombine(hi,(prev,current) -> prev + current)
+                .thenApply(String::toUpperCase)
+                .join();
+
+        long endTime = System.currentTimeMillis();
+        System.out.println("Time Taken ::\t" + (endTime - startTime));
+        return result;
+    }
+
+    public String helloWorld_with_3_async_with_exeptionally_func(){
+        long startTime = System.currentTimeMillis();
+        // Traditionally we call hello() and world() methods and Concatenate there results h + w
+        CompletableFuture<String> hello = CompletableFuture
+                .supplyAsync(() -> helloWorldService.hello());
+        CompletableFuture<String> world = CompletableFuture.
+                supplyAsync(() -> helloWorldService.world());
+        CompletableFuture<String> hi = CompletableFuture
+                .supplyAsync(() -> " Hi Completable Future");
+
+
+        String result = hello
+                .exceptionally((e) -> {
+                        System.out.println("Exception occur::" + e.getMessage());
+                        return ""; // recover value, as handle() is recover
+                })
+                .thenCombine(world, (h,w) -> h+w) // where h is the return of hello and w is the return of world
+                .exceptionally((e) -> {
+                        System.out.println("Exception on world method failed occur::" + e.getMessage());
+                        return ""; // recover value, as handle() is recover
+                  })
                 .thenCombine(hi,(prev,current) -> prev + current)
                 .thenApply(String::toUpperCase)
                 .join();
--- src/test/java/com/amarpreet/completableFuture/CompletableFutureHelloWorldExceptionTest.java
+++ src/test/java/com/amarpreet/completableFuture/CompletableFutureHelloWorldExceptionTest.java
@@ -25,7 +25,7 @@ class CompletableFutureHelloWorldExceptionTest {
                 .thenThrow(new RuntimeException("Exception occurred"));
         when(helloWorldService.world()).thenCallRealMethod();
 
-        String result = worldException.helloWorld_with_3_async();
+        String result = worldException.helloWorld_with_3_async_with_handle_func();
 
         assertEquals(" WORLD! HI COMPLETABLE FUTURE",result);
 
@@ -38,7 +38,7 @@ class CompletableFutureHelloWorldExceptionTest {
         when(helloWorldService.world())
                 .thenThrow(new RuntimeException("Exception occurred on world method as well"));
 
-        String result = worldException.helloWorld_with_3_async();
+        String result = worldException.helloWorld_with_3_async_with_handle_func();
 
         assertEquals(" HI COMPLETABLE FUTURE",result);
 
@@ -47,13 +47,50 @@ class CompletableFutureHelloWorldExceptionTest {
     @Test
     void helloWorld_with_3_async_hello_world_happy_path() {
         when(helloWorldService.hello())
-                .thenThrow(new RuntimeException("Exception occurred"));
+                .thenCallRealMethod();
         when(helloWorldService.world())
-                .thenThrow(new RuntimeException("Exception occurred on world method as well"));
+                .thenCallRealMethod();
 
-        String result = worldException.helloWorld_with_3_async();
+        String result = worldException.helloWorld_with_3_async_with_handle_func();
+
+        assertEquals("HELLO WORLD! HI COMPLETABLE FUTURE",result);
+
+    }
+
+    @Test
+    void helloWorld_with_3_async_exeptionally_func() {
+        when(helloWorldService.hello())
+                .thenThrow(new RuntimeException("Exception occurred"));
+        when(helloWorldService.world()).thenCallRealMethod();
+
+        String result = worldException.helloWorld_with_3_async_with_exeptionally_func();
+
+        assertEquals(" WORLD! HI COMPLETABLE FUTURE",result);
+
+    }
+
+    @Test
+    void helloWorld_with_3_async_exeptionally_func_2() {
+        when(helloWorldService.hello())
+                .thenThrow(new RuntimeException("Exception occurred"));
+        when(helloWorldService.world()).thenThrow(new RuntimeException("Again Exception occurred"));
+
+        String result = worldException.helloWorld_with_3_async_with_exeptionally_func();
 
         assertEquals(" HI COMPLETABLE FUTURE",result);
+
+    }
+
+    @Test
+    void helloWorld_with_3_async_hello_world_happy_path_exceptionally() {
+        when(helloWorldService.hello())
+                .thenCallRealMethod();
+        when(helloWorldService.world())
+                .thenCallRealMethod();
+
+        String result = worldException.helloWorld_with_3_async_with_exeptionally_func();
+
+        assertEquals("HELLO WORLD! HI COMPLETABLE FUTURE",result);
 
     }
 }
